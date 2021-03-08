b62521ab2e19284017da9ffbe0b32b3a8e2e99e7
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-08 11:11:53 +0530

    Anurag Prakash : has Added

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UC2_LadderSnake2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int choice = random.Next(0, 3);
+            if (choice == 0)
+                Console.WriteLine("no movement");
+            else if (choice == 1)
+                Console.WriteLine("is ladder player move a head");
+            else if (choice == 2)
+                Console.WriteLine("is snake player move back");
+        }
+    }
+}
