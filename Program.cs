1061e9c3b6fab4957351ad59744f59a18596790a
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-08 11:09:14 +0530

    Anurag Prakash ; has added

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC2_LadderSnake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            Console.WriteLine(dice);
+            
+        }
+    }
+}
